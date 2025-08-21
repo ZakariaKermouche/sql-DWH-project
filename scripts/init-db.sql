ae2fbe51a71229f99ae43c327a175207b15198a0
Zakaria Abdelmoumen KERMOUCHE <hz_kermouche@esi.dz>
2025-08-21 16:17:55 +0200

    add header to init-db.sql

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -1,4 +1,16 @@
-
+-------------------------------------------------------------------------------
+-- Script: init-db.sql
+-- Purpose: Initialize PostgreSQL database for Data Warehouse project
+-- Author: Zakaria
+-- Date: 2025-08-21
+--
+-- WARNING:
+-- 1. This script will DROP the database 'my_database' if it exists. 
+--    **All existing data in this database will be permanently lost.**
+-- 2. After dropping, it will create a new database 'DataWarehouse'.
+-- 3. The script then creates three schemas: BRONZE, SILVER, GOLD.
+-- 4. \connect works only in psql (terminal), not in pgAdmin Query Tool.
+-------------------------------------------------------------------------------
 
 
 -- Drop the database if it exists
