66a86ea4788cae9142beba2cf9b277d462bbb202
Zakaria Abdelmoumen KERMOUCHE <hz_kermouche@esi.dz>
2025-08-21 16:20:58 +0200

    Update init-db.sql

--- scripts/init-db.sql
+++ scripts/init-db.sql
@@ -5,12 +5,15 @@
 -- Date: 2025-08-21
 --
 -- WARNING:
--- 1. This script will DROP the database 'my_database' if it exists. 
+-- 1. This script will DROP the database 'DataWarehouse' if it exists. 
 --    **All existing data in this database will be permanently lost.**
--- 2. After dropping, it will create a new database 'DataWarehouse'.
--- 3. The script then creates three schemas: BRONZE, SILVER, GOLD.
--- 4. \connect works only in psql (terminal), not in pgAdmin Query Tool.
+-- 2. It is strongly recommended to take a full backup of 'DataWarehouse' 
+--    before executing this script.
+-- 3. After dropping, it will create a new database 'DataWarehouse'.
+-- 4. The script then creates three schemas: BRONZE, SILVER, GOLD.
+-- 5. \connect works only in psql (terminal), not in pgAdmin Query Tool.
 -------------------------------------------------------------------------------
+
 
 
 -- Drop the database if it exists
@@ -18,7 +21,7 @@ DO
 $$
 BEGIN
    IF EXISTS (SELECT FROM pg_database WHERE datname = 'my_database') THEN
-      EXECUTE 'DROP DATABASE my_database';
+      EXECUTE 'DROP DATABASE DataWarehouse';
    END IF;
 END
 $$ LANGUAGE plpgsql;
